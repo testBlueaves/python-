bc047a4890fa1f1e77aa958e9543aac6a7c45c5e
ashish Bindra <ashishbindra2@gmail.com>
2023-06-14 10:36:57 +0530

    seprate logger testing done

--- logging/student.py
+++ logging/student.py
@@ -1,10 +1,21 @@
 import logging
 
-logger = logging.gotLogger('studentLogger')
+logger = logging.getLogger('studentLogger')
 logger.setLevel(logging.DEBUG)
 
 fileHandler = logging.FileHandler('student.log',mode = 'w')
 fileHandler.setLevel(logging.ERROR)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s',datefmt = '%d - %m - %y %I:%M:%S %p)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s',
+								datefmt = '%d - %m - %y %I:%M:%S %p')
+
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+
+logger.debug('deug message from student')
+logger.info('Info message from student')
+logger.warning('Warning message from student')
+logger.error('Error message from student')
+logger.critical('Critical message from student')
+
 
